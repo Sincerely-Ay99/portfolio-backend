dd5355a1d15dcd4c0cf975c445783f89dc1a3222
Sincerely-Ay99 <ayodejiadu7@gmail.com>
2026-01-21 07:09:39 -0800

    Make EMAIL_PORT env var safe

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 CORS(app)
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USER = os.getenv("EMAIL_USER")
 EMAIL_PASS = os.getenv("EMAIL_PASS")
 RECEIVER_EMAIL = os.getenv("RECEIVER_EMAIL")
